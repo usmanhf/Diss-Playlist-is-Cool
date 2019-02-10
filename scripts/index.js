13d6777ddfe95059c3f10e62eba49e59c1132a8d
keanemind <keanemind@gmail.com>
2019-02-10 16:03:56 -0600

    Build base uri to prevent problems
    Problems like window.location.href being 'https://blah/?';
    the question mark will mess up the if statement.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 var redirect = function () {
     const client_id = 'a306c77b15cc46b7a80ed913f4a4a206';
-    const redirect_uri = encodeURIComponent(window.location.href + '/callback');
+    const base_uri = (
+        window.location.protocol + '//' + window.location.hostname + window.location.pathname
+    ); // needs testing with browsers other than Chrome
+    // window.location.pathname includes leading slash?
+    // window.location.hostname trailing slash?
+    const redirect_uri = encodeURIComponent(base_uri + 'callback');
     const scope = encodeURIComponent('playlist-modify-public playlist-modify-private');
     const response_type = 'token';
     const state = encodeURIComponent(document.getElementById('text-field').value);
